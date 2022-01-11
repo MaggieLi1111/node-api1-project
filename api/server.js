9e69fe4d37f366234ab3354c4d4df68ead5f134e
unknown <lidanyang1111@gmail.com>
2022-01-10 16:19:05 -0800

    version3

--- api/server.js
+++ api/server.js
@@ -7,22 +7,59 @@ const server = express()
 server.use(express.json())
 
 
+server.get("/api/users", (req, res) => {
+    User.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            console.log(err.message)
+            res.status(500).json({
+                message: "The users information could not be retrieved",
+                err: err.message,
+                stack: err.stack
+            })
+        })
+})
+
+server.get("/api/users/:id", (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            } else {
+                res.status(200).json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The user information could not be retrieved",
+                err: err.message,
+                stack: err.stack
+            })
+        })
+})
+
+
 server.post("/api/users", (req, res) => {
-    const newUser = req.body
-    if (!newUser.name || newUser.bio) {
+    const newUser = req.body;
+    if (!newUser.name || !newUser.bio) {
         res.status(400).json({
             message: "Please provide name and bio for the user",
         })
     } else {
         User.insert(newUser)
-            .then(newUser => {
-                console.log(newUser)
-                res.status(201).json(newUser)
+            .then(createdUser => {
+                console.log(createdUser)
+                res.status(201).json(createdUser)
             })
             .catch(err => {
                 res.status(500).json({
                     message: "There was an error while saving the user to the database",
-                    err: err.message
+                    err: err.message,
+                    stack: err.stack
                 })
             })
     }
@@ -42,8 +79,8 @@ server.put("/api/users/:id", async (req, res) => {
                     message: "Please provide name and bio for the user"
                 })
             } else {
-                const updateUser = await User.update(req.params.id, req.body)
-                res.status(200).json(updateUser)
+                const updatedUser = await User.update(req.params.id, req.body)
+                res.status(200).json(updatedUser)
             }
         }
     } catch (err) {
@@ -73,39 +110,11 @@ server.delete("/api/users/:id", async (req, res) => {
         res.status(500).json({
             message: "The user could not be removed",
             err: err.message,
+            stack: err.stack
         })
     }
 })
 
-server.get("/api/users", (req, res) => {
-    User.find()
-        .then(users => {
-            res.status(200).json(users)
-        })
-        .catch(err => {
-            console.log(err.message)
-            res.status(500).json({ message: "The users information could not be retrieved" })
-        })
-})
-
-server.get("/api/users/:id", (req, res) => {
-    User.findById(req.params.id)
-        .then(user => {
-            if (!user) {
-                res.status(404).json({
-                    message: "The user with the specified ID does not exist"
-                })
-            } else {
-                res.status(200).json(user)
-            }
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: "The user information could not be retrieved",
-                err: err.message
-            })
-        })
-})
 
 server.use("*", (req, res) => {
     res.status(404).json({
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const server = require('./api/server');
 
 const port = 9000;
 
+// console.log("hey you!");
+
 // START YOUR SERVER HERE
 server.listen(port, () => {
     console.log(`server running on http://localhost:${port}`);
